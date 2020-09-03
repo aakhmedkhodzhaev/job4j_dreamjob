6b580717964225f4bcd1fa57f405b198b284f508
aakhmedkhodzhaev <teenager_08@inbox.ru>
2020-09-03 21:22:10 +0500

    10. Ajax dynamic List and Upload Images

--- src/main/java/ru/job4j/dream/servlets/AjaxServlets.java
+++ src/main/java/ru/job4j/dream/servlets/AjaxServlets.java
@@ -1,15 +1,24 @@
 package ru.job4j.dream.servlets;
 
 import com.google.gson.Gson;
+import org.apache.commons.fileupload.FileItem;
+import org.apache.commons.fileupload.FileUploadException;
+import org.apache.commons.fileupload.disk.DiskFileItemFactory;
+import org.apache.commons.fileupload.servlet.ServletFileUpload;
 import ru.job4j.dream.model.City;
 import ru.job4j.dream.store.PsqlStore;
 
+import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.BufferedInputStream;
+import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.Collection;
+import java.util.List;
 
 
 public class AjaxServlets extends HttpServlet {
@@ -29,11 +38,40 @@ public class AjaxServlets extends HttpServlet {
     }
 
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        String city = req.getParameter("city");
+     /* String city = req.getParameter("city");
         String txt = "Вы выбрали город : " + city;
         resp.setContentType("text/plain");
         resp.setCharacterEncoding("UTF-8");
-        resp.getWriter().write(txt);
-    }
+        resp.getWriter().write(txt);*/
 
+        DiskFileItemFactory factory = new DiskFileItemFactory();
+        ServletContext servletContext = this.getServletConfig().getServletContext();
+        File repository = (File) servletContext.getAttribute("javax.servlet.context.tempdir");
+        factory.setRepository(repository);
+        ServletFileUpload upload = new ServletFileUpload(factory);
+        try {
+            List<FileItem> items = upload.parseRequest(req);
+            File folder = new File("images");
+            if (!folder.exists()) {
+                folder.mkdir();
+            }
+            for (FileItem item : items) {
+                if (!item.isFormField()) {
+                    File file = new File(folder + File.separator + item.getName());
+                    try (FileOutputStream out = new FileOutputStream(file);
+                         BufferedInputStream in = new BufferedInputStream(item.getInputStream())
+                    ) {
+                        byte[] dataBuffer = new byte[1024];
+                        int bytesRead;
+                        while ((bytesRead = in.read(dataBuffer, 0, dataBuffer.length)) != -1) {
+                            out.write(dataBuffer, 0, bytesRead);
+                        }
+                    }
+                }
+            }
+        } catch (FileUploadException e) {
+            e.printStackTrace();
+        }
+        doGet(req, resp);
+    }
 }
--- src/main/webapp/ajax.jsp
+++ src/main/webapp/ajax.jsp
@@ -1,37 +1,62 @@
 <%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
 <%@ page contentType="text/html; charset=UTF-8"
-         pageEncoding="ISO-8859-1"%>
+         pageEncoding="ISO-8859-1" %>
 <html>
 <head>
     <title>Test Ajax List of City</title>
     <script src="http://code.jquery.com/jquery-latest.min.js"></script>
     <script>
-        $(document).on("click", "#buttonLoad", function() {
-            $.get("ajax", function(responseJson) {
+        $(document).on("click", "#buttonLoad", function () {
+            $.get("ajax", function (responseJson) {
                 var $select = $("#Citylist");
                 $select.find("option").remove();
-                $.each(responseJson, function(index, city) {
+                $.each(responseJson, function (index, city) {
                     $("<option>").val(city.id).text(city.name).appendTo($select);
                 });
 
             });
         });
-
-        $(document).on("click", "#buttonSubmit", function() {
-            var params = {city : $("#Citylist option:selected").text()};
-            $.post("ajax", $.param(params), function(responseText) {
-                alert(responseText);
-            });
-        });
+        /*
+                $(document).on("click", "#buttonSubmit", function () {
+                    var params = {city: $("#Citylist option:selected").text()};
+                    $.post("ajax", $.param(params), function (responseText) {
+                        alert(responseText);
+                    });
+                });*/
     </script>
 </head>
 <body>
 <div align="center">
     <h2>Test Ajax List of City</h2>
+    <td>
+        <tr>
+            Name: <input id="candidate" name="candidate" placeholder="name"/>
+        </tr>
+    </td>
+    <br>
+    <br>
+    <td>
+        <tr>
+            Email: <input id="email" name="email" placeholder="email"/>
+        </tr>
+    </td>
+    <br>
+    <br>
     <button id="buttonLoad">Load</button> &nbsp;
     <select id="Citylist"></select>
-    <br/><br/>
-    <button id="buttonSubmit">Submit</button>
+    <br/>
+    <p>Upload Image:</p>
+    <form action="/items/ajax" method="post" enctype="multipart/form-data">
+        <div class="check-box" align="center">
+            <td>
+                <tr>
+                    <input type="file" id="file" name="file"/>
+                </tr>
+            </td>
+        </div>
+        </br>
+        <button type="submit" id="buttonSubmit">Submit</button>
+    </form>
 </div>
 </body>
 </html>
