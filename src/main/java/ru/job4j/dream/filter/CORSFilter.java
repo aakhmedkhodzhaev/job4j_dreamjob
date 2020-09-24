886e0cef9b1599d284ad5ba38eefd5747ce130cb
aakhmedkhodzhaev <teenager_08@inbox.ru>
2020-09-24 21:35:04 +0500

    6. CORS[305092#329613]

---
+++ src/main/java/ru/job4j/dream/filter/CORSFilter.java
@@ -0,0 +1,45 @@
+package ru.job4j.dream.filter;
+
+import java.io.IOException;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class CORSFilter implements Filter {
+
+    public CORSFilter() {
+    }
+
+    public void destroy() {
+    }
+
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain chain)
+            throws IOException, ServletException {
+
+        HttpServletRequest request = (HttpServletRequest) servletRequest;
+        System.out.println("CORSFilter HTTP Request: " + request.getMethod());
+
+        ((HttpServletResponse) servletResponse).addHeader("Access-Control-Allow-Origin", "*");
+        ((HttpServletResponse) servletResponse).addHeader("Access-Control-Allow-Methods", "GET, OPTIONS, HEAD, PUT, POST");
+        ((HttpServletResponse) servletResponse).addHeader("Access-Control-Allow-Headers", "X-PINGOTHER, Origin, X-Requested-With, Content-Type, Accept");
+        ((HttpServletResponse) servletResponse).addHeader("Access-Control-Max-Age", "1728000");
+
+        HttpServletResponse resp = (HttpServletResponse) servletResponse;
+
+        if (request.getMethod().equals("OPTIONS")) {
+            resp.setStatus(HttpServletResponse.SC_ACCEPTED);
+            return;
+        }
+
+        chain.doFilter(request, servletResponse);
+    }
+
+    public void init(FilterConfig fConfig) throws ServletException {
+    }
+
+}
